dbc9c3be1ac15fc65c557102dd7c89b8fc97c2a8
Keenan Gee <k33nang33@hotmail.co.uk>
2023-07-12 13:12:18 +0100

    added more endpoints

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,30 @@
+import { Api, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        bind: [table],
+      },
+    },
+    routes: {
+      "GET /notes": "packages/functions/src/list.main",
+      "POST /notes": "packages/functions/src/create.main",
+      "GET /notes/{id}": "packages/functions/src/get.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
