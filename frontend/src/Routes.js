7f6216968375c9120220c627334e89c2a426c9eb
Keenan Gee <k33nang33@hotmail.co.uk>
2023-07-18 16:06:23 +0100

    added login and signup func, added post notes func, and started view notes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
       <Route path="*" element={<NotFound />} />;
     </Routes>
   );
